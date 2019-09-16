404e71f9bab080aa8d353737e6c398a9b3f85e2a
Andrew W King <kinglx@bc.edu>
2019-09-16 17:43:18 -0400

    Initial Commit

---
+++ Magic Eight Ball Q1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Eight Ball Q1
+//
+//  Created by Andrew King on 9/16/19.
+//  Copyright © 2019 Andrew King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
