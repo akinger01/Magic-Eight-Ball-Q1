3308784fac31c6ca6807e579edf8a172e5af4832
Andrew W King <kinglx@bc.edu>
2019-09-16 17:54:50 -0400

    pudding cups

--- Magic Eight Ball Q1/ViewController.swift
+++ Magic Eight Ball Q1/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var eightBallResponseLabel: UILabel!
+    
+    let responses = ["It is certain",
+                     "It is decidedly so",
+                     "Without a doubt",
+                     "Yes, definitely",
+                     "You may rely on it",
+                     "As I see it, yes",
+                     "Most likely",
+                     "Outlook good",
+                     "Yes",
+                     "Signs point to yes",
+                     "Reply hazy try again",
+                     "Ask again later",
+                     "Better not tell you now",
+                     "Cannot predict now",
+                     "Concentrate and ask again",
+                     "Don't count on it",
+                     "My reply is no",
+                     "My sources say no",
+                     "Outlook not so good",
+                     "Very doubtful"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerPressed(_ sender: UIButton) {
+        eightBallResponseLabel.text = responses.randomElement()!
+    }
+    
 }
 
